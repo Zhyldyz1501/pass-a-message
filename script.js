2d754b434a854057fe9d0baf86d0c19707fb8ebe
Zhyldyz <jargynbaeva@gmail.com>
2021-12-16 01:39:00 +0200

    func added

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const input = document.querySelector('#input');
 const button = document.querySelector('#btn');
 const p = document.querySelector('#message');
+let inputVal = '';
 
 button.addEventListener('click', clicked)
 function clicked (){
     let inputVal = input.value;
     p.innerHTML = inputVal;
-    p.style.visibility = 'visible'
+    input.value = '';
+    button.innerHTML = 'submitted'
+    }
+
+input.addEventListener('click', changed)
+function changed (){
+    button.innerHTML = 'submit'
 }
+
